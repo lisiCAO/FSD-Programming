1c74cb4948c1ac950f7ccff1262d03f936097e99
LisiCAO <caolisi@hotmail.com>
2023-08-10 10:08:21 -0400

    activity 6th july Pro2

--- Activity July6th/src/activity6thjuly/MyProgram1.java
+++ Activity July6th/src/activity6thjuly/MyProgram1.java
@@ -1,24 +1,30 @@
-//Purpose: use generic to compare two arrays that may not be the same types.
-//Author: Lisi Cao
+/*Purpose: use generic to compare two arrays with different types.
+ *Author: Lisi Cao
+*/
 
 package activity6thjuly;
 import java.util.Arrays;
 
 public class MyProgram1 {
 	public static <T> boolean equals(T[] array1, T[] array2) {
-		/*
-		if(array1.length == array2.length) {
-			for(int i = 0; i < array1.length;i++) {
-				if(!array1[i].equals(array2[i])) {
-					return false;
-				}
-			}
-			return true;
-		}return false;
+
+		return Arrays.equals(array1, array2); //orders and values should be exact same.
+		/** to compare regardless order
+		 * 		
+		 * Arrays.sort(array1);
+		 * Arrays.sort(array2);
+		 * 	if(array1.length == array2.length) {
+		 * for(int i = 0; i < array1.length;i++) {
+		 * 	if(!array1[i].equals(array2[i])) {
+		 * 			return false;
+		 * 		}
+		 * }
+		 * 	return true;
+		 * }return false;
 		*/
-		return Arrays.equals(array1, array2);
 		
 	}
+	
 
 	public static void main(String[] args) {
 		Integer[] array1 = { 1, 2, 3, 4, 5 };
--- Activity July6th/src/activity6thjuly/MyProgram2.java
+++ Activity July6th/src/activity6thjuly/MyProgram2.java
@@ -1,27 +1,48 @@
+/*
+ * Purpose: calculate the sum of even number and odd number separately 
+ * in a specific array.
+ * Author: Lisi Cao
+ * Method: Using generic List<>() would be flexible but limit to integer
+ * 	based on the purpose.
+ */
+
 package activity6thjuly;
 
 import java.util.List;
 
 public class MyProgram2 {
-	public static <T extends Number> int[] CalSumOfEvenOrOdd(List<T> numbers) {
-		int evenNum = 0;
-		int oddNum = 0;
-		for(T number : numbers ) {
-			if(number.intValue() % 2 == 0) {
-				evenNum += number.intValue();
-			}else {
-				oddNum += number.intValue();
+	/**
+	 * limit List() to Integer return sum of even and sum of odd in an array
+	 */
+	public static int[] CalSumOfEvenOrOdd(List<Integer> numbers) {
+		int evenSum = 0;
+		int oddSum = 0;
+		for (Integer number : numbers) {
+			// An even number divided by two leaves a remainder of zero.
+			if (number.intValue() % 2 == 0) {
+				evenSum += number;
+			}
+			// an integer is either even, or odd.
+			else {
+				oddSum += number;
 			}
 		}
-		return new int[] {evenNum,oddNum};
+		return new int[] { evenSum, oddSum }; // array with two results.
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<Integer> numbers = List.of(1,2,3,4,5,6,7,8,9,10);
+
+		// generate a list of integer number.
+		List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+		System.out.println("The list of numbers generated: " + numbers);
+
+		// call the method CalSumOfEevnOrOdd, and assign values to integer array sum
 		int[] sum = CalSumOfEvenOrOdd(numbers);
-		System.out.println("The sum of Even numver: " + sum[0]);
-		System.out.println("The sum of Even numver: " + sum[1]);
+
+		// print results clearly
+		System.out.println("The sum of even numver: " + sum[0]);
+		System.out.println("The sum of odd numver: " + sum[1]);
 
 	}
 
